fbe0f4cf040333481b179a68f4924f651b81a002
Brandon Lyon <etherous@gmail.com>
2020-06-18 12:09:22 -0600

    Added /conversation endpoint

--- src/main/java/com/chg/hackdays/chappie/db/repository/ConversationRepository.java
+++ src/main/java/com/chg/hackdays/chappie/db/repository/ConversationRepository.java
@@ -3,9 +3,13 @@ package com.chg.hackdays.chappie.db.repository;
 import com.chg.hackdays.chappie.db.entity.ConversationEntity;
 import com.chg.hackdays.chappie.db.entity.MessageEntity;
 import com.chg.hackdays.chappie.db.entity.TopicEntity;
+import com.chg.hackdays.chappie.db.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ConversationRepository extends JpaRepository<ConversationEntity, Long> {
+    Collection<ConversationEntity> findByParticipants(UserEntity participant);
 }
